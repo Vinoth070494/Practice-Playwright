b5f9e9d8ec3e7077854f1fdbe86ce6c41cb3314c
Vinoth070494 <vinothvolley06@gmail.com>
2026-04-02 16:44:10 +0530

    update of DD

--- tests/Dd.spec.ts
+++ tests/Dd.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test';
 
-test('Drag and Drop ', async ({ page }) => {
+ test('Drag and Drop ', async ({ page }) => {
 
   await page.goto('https://practice.expandtesting.com/drag-and-drop');
 
@@ -13,8 +13,21 @@ test('Drag and Drop ', async ({ page }) => {
   await expect(source).toContainText('B');
    await expect(target).toContainText('A');
    await page.waitForTimeout(2000);
+}); 
 
-
-
-
+test('Drag and Drop Circles', async ({ page }) => {
+ 
+  // Open your page
+  await page.goto('https://practice.expandtesting.com/drag-and-drop-circles');
+ 
+  // Locators
+  const greenCircle = page.locator('.green');      // change if needed
+  const dropBox = page.locator('#target');     // change if needed
+ 
+  // Drag and drop
+  await greenCircle.dragTo(dropBox);
+ 
+  // Optional validation
+  //await expect(dropBox).toContainText('Dropped'); // depends on UI
 });
+
