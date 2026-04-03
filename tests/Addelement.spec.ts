98a626d2f118397c6e1d2e4481ec0919b4159b6a
Vinoth070494 <vinothvolley06@gmail.com>
2026-04-03 16:37:28 +0530

    new tasks

---
+++ tests/Addelement.spec.ts
@@ -0,0 +1,15 @@
+import { test, expect } from '@playwright/test';
+ 
+test('Add element dynamic ', async ({ page }) => {
+ 
+  await page.goto('https://practice.expandtesting.com/add-remove-elements');
+
+ await page.getByRole("button" ,{ name:"Add Element"}).click();
+ 
+const deleteButton = page.getByRole('button', { name: 'Delete' });
+  await expect(deleteButton).toBeVisible();
+  await page.getByRole('button', { name: 'Delete' }).click();
+    await expect(deleteButton).toHaveCount(0);
+await page.pause();
+
+});
---
+++ tests/Filedownload.spec.ts
@@ -0,0 +1,21 @@
+import { test, expect } from '@playwright/test';
+ 
+test('File Upload Test', async ({ page }) => {
+ 
+  await page.goto('https://practice.expandtesting.com/download');
+
+  // Wait for download event
+const downloadPromise = page.waitForEvent('download');
+ 
+// Click on file link
+await page.click('#1775133072702_DNDAgentFile.txt');
+ 
+// Capture download
+const download = await downloadPromise;
+ 
+// Print file name
+console.log(await download.suggestedFilename());
+ 
+// Save file
+await download.saveAs('vinoth/DNDagentfile.txt');
+});
